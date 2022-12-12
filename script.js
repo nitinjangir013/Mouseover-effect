319a733140c6517bd9e25149f7270f286f5fe0f9
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2022-12-11 19:51:56 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+// Card First
+$(".card_1").hover(function(){
+  $(this).css({
+    filter:"grayscale(0%)",
+    color:'#fff',
+    'margin-top': '-20px',
+    'box-shadow': '0 10px 10px 10px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d1").css({
+    'margin-top':'10px'
+  });
+  $(".button_1").text('Click here ->');
+  }, function(){
+  $(this).css({
+    filter:"grayscale(100%)",
+    color:'#000',
+    'margin-top': '0px',
+    'box-shadow': '0 0px 10px 0px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d1").css({
+    'margin-top':'30px'
+  });
+  $(".button_1").text('');
+});
+
+
+// Card Secound
+$(".card_2").hover(function(){
+  $(this).css({
+    filter:"grayscale(0%)",
+    color:'#fff',
+    'margin-top': '-20px',
+    'box-shadow': '0 10px 10px 10px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d2").css({
+    'margin-top':'10px'
+  });
+  $(".button_2").text('Click here ->');
+  }, function(){
+  $(this).css({
+    filter:"grayscale(100%)",
+    color:'#000',
+    'margin-top': '0px',
+    'box-shadow': '0 0px 10px 0px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d2").css({
+    'margin-top':'30px'
+  });
+  $(".button_2").text('');
+});
+
+
+// Card Third
+$(".card_3").hover(function(){
+  $(this).css({
+    filter:"grayscale(0%)",
+    color:'#fff',
+    'margin-top': '-20px',
+    'box-shadow': '0 10px 10px 10px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d3").css({
+    'margin-top':'10px'
+  });
+  $(".button_3").text('Click here ->');
+  }, function(){
+  $(this).css({
+    filter:"grayscale(100%)",
+    color:'#000',
+    'margin-top': '0px',
+    'box-shadow': '0 0px 10px 0px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d3").css({
+    'margin-top':'30px'
+  });
+  $(".button_3").text('');
+});
+
+
+// Card Four
+$(".card_4").hover(function(){
+  $(this).css({
+    filter:"grayscale(0%)",
+    color:'#fff',
+    'margin-top': '-20px',
+    'box-shadow': '0 10px 10px 10px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d4").css({
+    'margin-top':'10px'
+  });
+  $(".button_4").text('Click here ->');
+  }, function(){
+  $(this).css({
+    filter:"grayscale(100%)",
+    color:'#000',
+    'margin-top': '0px',
+    'box-shadow': '0 0px 10px 0px rgba(0, 0, 0, 0.1)'
+  });
+  $(".i_d4").css({
+    'margin-top':'30px'
+  });
+  $(".button_4").text('');
+});
